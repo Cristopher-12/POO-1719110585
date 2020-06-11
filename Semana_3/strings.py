733e24c6530653f1148ffcee2ef61571c4d7a800
Cristopher Gonzalez <1719110585@utectulancingo.edu.mx>
2020-06-11 03:49:56 +0000

    STRING

---
+++ Semana_3/strings.py
@@ -0,0 +1,8 @@
+leercadena = str(input("Introduce cadena a leer: "))
+
+# Tipo de cadena #
+print(leercadena.isnumeric())
+print(leercadena.isalpha())
+print(leercadena.isdigit())
+# Longitud de Cadena
+print(len(leercadena))
